2ff1617bedf02d654331c60c2ac0abbc8d502bd8
Singhjoti <133053295+Singhjoti@users.noreply.github.com>
2024-03-19 18:24:16 +0000

    iteration or loop in js

---
+++ 05-iteration/three.js
@@ -0,0 +1,41 @@
+//for of
+//["", "", ""]
+//[{},{},{}]
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings = "Hello world!"
+for (const greet of greetings) {
+    console.log(`Each char is ${greet}`)
+}
+
+// Maps
+// for of loop
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States of America")
+map.set('Fr', "France")
+map.set('IN', "India")
+
+console.log(map);
+/*
+
+// for of loop
+for (const [key, value] of map) { agr app only key lenge to arry me pura print hoga agr aap [key value ddale ge to sirf value print hoga]
+    // console.log(key, ':-', value);
+}
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+    
+// }
+ */
